48242125958615e801cd3d871141515f08b59ac9
xumenger <xumenger@126.com>
2018-07-09 17:32:50 +0800

    update test cases

--- Sources/test.cc
+++ Sources/test.cc
@@ -8,11 +8,11 @@ int main()
 {
     cout << "false = " << false << endl;
     cout << "true  = " << true << endl;
-    cout << match("aaa", "a.a") << endl;
-    cout << match("aaa", "ab*ac*a") << endl;
-    cout << match("aaa", "aa.a") << endl;
-    cout << match("aaa", "ab*.a") << endl;
-    cout << match("aaa", "ab*a") << endl;
+    cout << match("aaa", "a.a") << " = " << true << endl;
+    cout << match("aaa", "ab*ac*a") << " = " << true << endl;
+    cout << match("aaa", "aa.a") << " = " << false << endl;
+    cout << match("aaa", "ab*.a") << " = " << true << endl;
+    cout << match("aaa", "ab*a") << " = " << false << endl;
 
     return 0;
 }
