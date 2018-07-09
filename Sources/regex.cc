5467213fcd11a601b715c383baa23b034f4705f4
xumenger <xumenger@126.com>
2018-07-09 17:34:32 +0800

    use state machine and recursion

--- Sources/regex.cc
+++ Sources/regex.cc
@@ -3,7 +3,21 @@
 
 bool matchCore(const char *str, const char *pattern)
 {
+    if('\0' == *str && '\0' == *pattern)
+        return true;
+    if('\0' != *str && '\0' == *pattern)
+        return false;
 
+    if('*' == *(pattern + 1)){
+        if(*pattern == *str || *pattern == '.')
+            return matchCore(str + 1, pattern) || matchCore(str + 1, pattern + 2);
+        else
+            return matchCore(str, pattern + 2);
+    }
+    else if(*str == *pattern || *pattern == '.')
+        return matchCore(str + 1, pattern + 1);
+
+    return false;
 }
 
 bool match(const char *str, const char *pattern)
