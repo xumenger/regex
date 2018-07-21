17635b6ee46d83b17afe4b663c27b3ae1e5e5483
xumenger <xumenger@126.com>
2018-07-21 22:18:49 +0800

    Repeat to_nfa_design

--- more/Regex-Ruby/Regex/Pattern.rb
+++ more/Regex-Ruby/Regex/Pattern.rb
@@ -127,4 +127,16 @@ class Repeat < Struct.new(:pattern)
     def precedence
         2
     end
+
+    def to_nfa_design
+        pattern_nfa_design = pattern.to_nfa_design
+
+        start_state = Object.new
+        accept_states = pattern_nfa_design.accept_states + [start_state]
+        rules = pattern_nfa_design.rulebook.rules
+        extra_rules = pattern_nfa_design.accept_states.map{ |accept_state| FARule.new(accept_state, nil, pattern_nfa_design.start_state) } + [FARule.new(start_state, nil, pattern_nfa_design.start_state)]
+        rulebook = NFARulebook.new(rules + extra_rules)
+
+        NFADesign.new(start_state, accept_states, rulebook)
+    end
 end
