29b940ce91eb21ed86d2e67dcd552fca266810e2
xumenger <xumenger@126.com>
2018-07-21 20:38:34 +0800

    NFA class

--- more/Regex-Ruby/Regex/NFA.rb
+++ more/Regex-Ruby/Regex/NFA.rb
@@ -6,6 +6,10 @@
 require 'set'
 require "./FARule.rb"
 
+
+# 存储NFA 规则的规则手册
+# 当我们询问DFA 规则手册处于特定状态的DFA读到一个特定的字符之后下一步应该转到何处时，总会返回一个特定状态
+# 但NFA 规则手册需要回答一个不同的问题：在NFA 处于集中可能状态之一是，它读取到一个特定的字符，可能的下一个状态是什么？
 class NFARulebook < Struct.new(:rules)
     def next_states(states, character)
         states.flat_map { |state| follow_rules_for(state, character) }.to_set
@@ -21,6 +25,14 @@ class NFARulebook < Struct.new(:rules)
 end
 
 
+# 实现一个NFA 类来模拟非确定有限状态自动机
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+    def accepting?
+        (current_states & accept_states).any?
+    end
+end
+
+
 print("## test NFARulebook\n")
 rulebook = NFARulebook.new([
     FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
@@ -29,3 +41,7 @@ rulebook = NFARulebook.new([
 ])
 p rulebook.next_states(Set[1], 'b')
 p rulebook.next_states(Set[1, 2], 'a')
+
+print("\n## test NFA\n")
+p NFA.new(Set[1], [4], rulebook).accepting?
+p NFA.new(Set[1, 2, 4], [4], rulebook).accepting?
