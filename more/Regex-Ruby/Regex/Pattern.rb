a014f1c54047702e66862ca4da68c618f28a5918
xumenger <xumenger@126.com>
2018-07-21 22:05:57 +0800

    Concatenate

--- more/Regex-Ruby/Regex/Pattern.rb
+++ more/Regex-Ruby/Regex/Pattern.rb
@@ -75,6 +75,19 @@ class Concatenate < Struct.new(:first, :second)
     def precedence
         1
     end
+
+    def to_nfa_design
+        first_nfa_design = first.to_nfa_design
+        second_nfa_design = second.to_nfa_design
+
+        start_state = first_nfa_design.start_state
+        accept_states = second_nfa_design.accept_states
+        rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+        extra_rules = first_nfa_design.accept_states.map { |state| FARule.new(state, nil, second_nfa_design.start_state) }
+        rulebook = NFARulebook.new(rules + extra_rules)
+
+        NFADesign.new(start_state, accept_states, rulebook)
+    end
 end
 
 
